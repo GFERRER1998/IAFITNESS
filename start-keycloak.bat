381d2ec676033b682fd61a7a7efecbe27b33f277
GFERRER1998 <gnzlferrer@gmail.com>
2026-03-30 23:13:11 -0300

    Cleanup: Keep only folders, README, and RECRUITER_SUMMARY in root

--- start-keycloak.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set KEYCLOAK_ADMIN=admin
-set KEYCLOAK_ADMIN_PASSWORD=admin
-echo Iniciando Keycloak 26.1.0 en puerto 7080...
-echo Usuario: admin
-echo Password: admin
-C:\keycloak\bin\kc.bat start-dev --http-port 7080
-pause
